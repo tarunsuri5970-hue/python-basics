d774c39f7f05741b90bc058531f50c1f0f23f787
tarunsuri5970-hue <tarunsuri5970@gmail.com>
2026-07-11 22:40:18 +0530

    change in code

--- chapter 1-variable and datatypes/annualincometax.py
+++ chapter 1-variable and datatypes/annualincometax.py
@@ -1,11 +1,18 @@
 annualincome = float(input("enter your annual income:"))
-if annualincome <=50000 :
-    print("tax:"(annualincome*0.5))
-elif annualincome <=100000 :
-    print("tax:"(annualincome*0.5))
-elif annualincome <=100000 :
-    print("tax:"(annualincome*0.5))
-elif annualincome <=100000 :
-    print("tax:"(annualincome*0.5))
-elif annualincome <=100000 :
-    print("tax:"(annualincome*0.5))
+if annualincome <=400000 :
+    tax = 0
+elif annualincome <=800000 :
+    tax=annualincome*0.05
+elif annualincome <=1200000 :
+    tax=annualincome*0.1
+elif annualincome <=1600000 :
+    tax=annualincome*0.15
+elif annualincome <=2000000 :
+    tax=annualincome*0.2
+elif annualincome <=2400000 :
+    tax=annualincome*0.25
+else:
+    tax=annualincome*0.3
+print("your annual income is:",annualincome)
+print("your tax is:",tax)
+print("your remaining income is:",(annualincome-tax))
